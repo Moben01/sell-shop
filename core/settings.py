5504e49bc88982c765b8cbaa7a463ee1f77ab18c
shafiqalandari <shafifazli73@gmail.com>
2025-11-13 13:59:24 +0430

    Switch database backend to PostgreSQL
    Updated the Django database configuration from SQLite to PostgreSQL, specifying connection details for the new backend. The previous SQLite configuration is now commented out for reference.

--- core/settings.py
+++ core/settings.py
@@ -99,12 +99,27 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sellshop',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',  # Replace with your PostgreSQL server's address if necessary
+        'PORT': '5432',          # Leave empty to use the default PostgreSQL port (usually 5432)
     }
 }
+
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
